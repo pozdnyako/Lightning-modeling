7394d5a66495ed58f7ee7529c6a5678fbb9fad63
pozdnyako <pozdnyak2001@mail.ru>
2020-03-21 00:04:55 +0300

    updateGPU

--- Solver.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOLVER_H_INCLUDED
-#define SOLVER_H_INCLUDED
-
-#include "Matrix.h"
-
-void solve(Matrix A, Matrix B, Matrix* ans); // A * ans = B, finding ans
-
-#endif // SOLVER_H_INCLUDED
